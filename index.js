57b04ce97ff0c2a0aa1b5686ba0b48e60644ebcd
joao <joaopedromestreff2007@gmail.com>
2023-11-25 21:48:29 -0300

    adição dos icones de lua e de sol para a troca de tema

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
 
-
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
 
 app.get('/', (requisicao, resposta) =>{
     resposta.render('home')
