a98bf5db6816c20e9244a1b6dbcc2ae77495bd6f
joao <joaopedromestreff2007@gmail.com>
2023-11-26 22:29:59 -0300

    exibição das tarefas na pagina home concluida

--- index.js
+++ index.js
@@ -52,9 +52,8 @@ app.get('/', (requisicao, resposta) =>{
             }
         })
 
+        resposta.render('home', { tarefas })
     })
-
-    resposta.render('home')
 })
 
 const conexao = mysql.createConnection({
